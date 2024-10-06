c3a4d32a3a7534aabd12fc1f4b7af7e75f399681
vishal0714 <122671523+vishal0714@users.noreply.github.com>
2024-10-06 22:09:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const byteSize = (str) => {
-  // write your code here
+    // Create a new Blob object with the given string
+    const blob = new Blob([str]);
+    // Return the size of the Blob in bytes
+    return blob.size;
 };
+
+// Examples
+console.log(byteSize('hello world')); // Output: 11
+console.log(byteSize('안녕하세요')); // Output: 15
+console.log(byteSize('')); // Output: 0
 
 // Do not change the code below
 const str = prompt("Enter some string.");
